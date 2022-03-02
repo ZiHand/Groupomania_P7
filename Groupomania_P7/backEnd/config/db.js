c5ebcabcdcc449a8670be5d019f7e16303ba9c97
Ziggy <ziggyhand.dev@gmail.com>
2022-03-02 20:29:19 +0100

    read me up

--- Groupomania_P7/backEnd/config/db.js
+++ Groupomania_P7/backEnd/config/db.js
@@ -10,36 +10,3 @@ const sequelize = new Sequelize("groupomania", process.env.DB_USER, process.env.
 //                Sequelize Export
 // ===================================================
 module.exports = sequelize;
-
-const UserModel         = require('../js/models/user_model');
-//const ModeratorModel    = require('../js/models/moderator_model');
-const PostModel         = require('../js/models/post_model');
-const CommentModel      = require('../js/models/comment_model');
-
-
-/*const db = {};
-
-db.Sequelize   = Sequelize;
-db.sequelize   = sequelize;
-
-db.user        = require("../js/models/user_model");(sequelize, Sequelize);
-db.post        = require('../js/models/post_model');(sequelize, Sequelize);
-db.comment     = CommentModel;
-db.moderator   = ModeratorModel;*/
-
-// We use hasMany() to help one User have many Posts, 
-// and belongsTo() to indicate that one Post only belongs to one User.
-//UserModel.hasMany(PostModel, { as: "posts" });
-//PostModel.belongsTo(UserModel, { foreignKey: "userId", as: "user" });
-
-/*db.post.hasMany(db.comment, { as: "comments" });
-db.comment.belongsTo(db.post, { foreignKey: "postId", as: "post" });
-
-db.user.hasMany(db.comment, { as: "comments" });
-db.comment.belongsTo(db.user, { foreignKey: "userId", as: "user" });
-
-db.user.belongsTo(db.moderator, { foreignKey: "modId", as: "moderator" });
-db.moderator.belongsTo(db.user, { foreignKey: "userId", as: "user" });
-
-module.exports = db;*/
-
--- Groupomania_P7/readme.md
+++ Groupomania_P7/readme.md
@@ -1,7 +1,25 @@
+---
+### Groupomania Private Social Network
+# React / Redux / Express / mySql
+---
+
 ### ===================== BACKEND ========================
+
+# Backend config:
+
+- Mettez vos informations dans `/backend/config/db.js`
+- Créez le fichier `.env` dans `/backend/config/` dans les données suivantes
+  - PORT=8080 `votre port localhost`
+  - DB_USER='the_user' `votre login`
+  - DB_PASS='the_password' `votre password`
+  - TOKEN_SECRET=990bf68e6adf1be5f1671bba3bec692056922454 `votre clé secrète`
+  - MODERATOR_SECRET_KEY=81e844b3-2d93-4f29-98a3-f5b9c18d6b91 `votre clé secrète pour les moderateurs (peut etre changée a tout moment, veulliez la communiquer a vos nouveaux moderateurs)`
 
 cd ./backend
 npm install
+
+# Backend start:
+
 nodemon server
 
 ### ==================== FRONTEND ========================
